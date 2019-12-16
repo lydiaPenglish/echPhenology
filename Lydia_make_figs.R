7fdd013cc4a06549737648eaa282e7e171b120c7
Lydia English <lydia.english@gmail.com>
2019-12-15 20:30:43 -0600

    Re-organizing data

--- Lydia_make_figs.R
+++ Lydia_make_figs.R
@@ -6,6 +6,7 @@
 library(tidyverse)
 library(patchwork) # for plottig multiple graphs
 library(lmerTest)
+library(viridis)
 load("data/phen_dataset.rda") #both 1996 and 1997 dataset
 theme_set(theme_bw())
 
@@ -49,6 +50,75 @@ p2 <- phen_19967 %>%
         legend.background = element_rect(color = "black"))
 
 p1 + p2
+phen_19967 <- phen_19967 %>%
+  mutate(yearN = as.numeric(year))
+# should year be a factor or continuous?
+m1 <- lmerTest::lmer(dur ~ headCt + year + expNm + (1|cgPlaId), data = phen_19967)
+summary(m1)
+anova(m1)
 
-l1 <- lmerTest::lmer(dur ~ headCt + (1|year), data = phen_19967)
+phen11 <- phen_19967 %>%
+  filter(year == 2011)
+
+l1 <- lm(dur ~ headCt + I(headCt^2), data = phen11)
 summary(l1)
+
+ggplot(phen_19967, aes(headCt, dur))+
+  geom_point(alpha = 0.2)+
+  facet_wrap(~year)
+
+# # Trying to visualize with plants are flowering each year and when they start # #
+
+# all row/pos
+
+rowpos <- read_csv("data-raw/cg1CoreData.csv") %>%
+  select(cgPlaId:yrPlanted) %>%
+  filter(yrPlanted == 1996 | yrPlanted == 1997) %>%
+  mutate(yrPlanted = as.factor(yrPlanted))
+
+phen_19967 %>%
+  filter(year != 2017) %>%
+  ggplot()+
+  geom_point(data = rowpos, aes(row, pos), size = 0.5)+
+  geom_point(aes(row, pos, color = dam))+
+  scale_color_viridis(option="magma")+
+  facet_wrap(~year)
+
+phen_19967 %>%
+  ggplot()+
+  #geom_point(data = rowpos, aes(row, pos), size = 0.5)+
+  geom_point(aes(row, pos, size = phenCt), color = "black", fill = "white")
+
+
+
+# # trying out a plot where I look at plants that flowered more than 7 times and their spread of flowering times
+# this one has standarized dates (mean +/- error bar)
+phen_19967 %>%
+  filter(phenCt > 7) %>%
+  group_by(cgPlaId) %>%
+  summarize(meanDAM = mean(dam),
+            sdDAM   = sd(dam),
+            n       = n(),
+            seDAM   = sdDAM/sqrt(n))%>%
+  mutate(newID = rank(meanDAM)) %>%
+  ggplot(aes(newID, meanDAM))+
+  geom_point()+
+  geom_errorbar(aes(ymin=meanDAM-seDAM, ymax=meanDAM+seDAM), colour="black", width=.1)
+
+# this one has boxplot - more full spread
+phen_7 <- phen_1996 %>%
+  filter(phenCt > 7) %>%
+  group_by(cgPlaId) %>%
+  mutate(medDAM = median(dam)) %>%
+  ungroup() %>%
+  distinct(cgPlaId, .keep_all = TRUE) %>%
+  arrange(desc(medDAM)) %>%
+  mutate(newID = row_number()) %>%
+  select(cgPlaId, medDAM, newID) %>%
+  left_join(., phen_1996, by = "cgPlaId")
+phen_7 %>% 
+  ggplot(aes(x = newID, y = dam)) +
+  geom_boxplot(aes(group = newID))+
+  #geom_point(alpha = 0.2)+
+  geom_hline(yintercept = 0, lty = 2) +
+  coord_flip() 
---
+++ Lydia_make_tidy_data.R
@@ -0,0 +1,133 @@
+# Script to tidy Echinacea phenology data and save as an .rda file or use in a markdown doc
+#       created by:    Lydia English
+#       last modified: 12/15/2019 by LE
+
+# # Load packages # # 
+
+library(tidyverse) # using tidy syntax
+library(lubridate) # to help with dates
+
+# # Load phenology datasets # #
+
+p.05.17x <- read_csv("data-raw/exPt1Phenology.csv")                 # data from 2005 - 2017
+p.10x <- read_delim("data-raw/2010.CG1.Phenology.txt", delim = ",") # 2010 data
+ped <- read_csv("data-raw/96979899qGenPedigreeLE.csv")              # pedigree data for site origins
+rowpos <- read_csv("data-raw/cg1CoreData.csv") %>%                  # row/position data
+  select(cgPlaId:yrPlanted) %>%                                     # using all of CG1 data since I only had row/pos for 1996
+  filter(yrPlanted == 1996 | yrPlanted == 1997) %>%
+  mutate(yrPlanted = as.factor(yrPlanted))
+# # Data wrangling # # 
+
+# 2010 data 
+p.10 <- p.10x %>%
+  dplyr::rename("year" = "Year")%>%
+  dplyr::rename("startDtEarly"= "startDateEarly")%>%
+  dplyr::rename("startDtLate" = "startDateLate")%>%
+  dplyr::rename("endDtEarly"  = "endDateEarly")%>%
+  dplyr::rename("endDtLate"   = "endDateLate")%>%
+  # converting dates from characters to dates 
+  mutate_at(vars(startDtEarly:endDtLate), lubridate::ymd_hms)%>%
+  mutate_at(vars(startDtEarly:endDtLate), lubridate::date)%>%
+  select(-note)
+
+# 2005 - 2017 data
+p.05.17 <- p.05.17x %>%
+  dplyr::rename("year" = "phenYear")%>%
+  group_by(year, cgPlaId) %>%
+  # getting number of heads, deleting cgHdId
+  dplyr::mutate(headCt = n()) %>%
+  ungroup() %>%
+  select(-c(row, pos, ttColor, phenNote, cgHdId))
+
+# need one row per plant - consolidating flowering phenology to earliest and latest dates
+#    if a plant has multiple heads
+
+# earliest starts
+starts <- p.05.17 %>%
+  group_by(year, cgPlaId)%>%
+  filter(startDtEarly == min(startDtEarly))%>%
+  select(year, cgPlaId, startDtEarly, headCt)
+# latest ends
+ends <-   p.05.17 %>%
+  group_by(year, cgPlaId)%>%
+  filter(endDtLate    == max(endDtLate)) %>%
+  select(year, cgPlaId, endDtLate, headCt)
+
+# join starts and ends back together
+p.05.17f <- left_join(starts, ends, by = c("year", "cgPlaId", "headCt")) %>%
+  arrange(year, cgPlaId) %>%
+  dplyr::distinct()%>%
+  # get rid of bad dates
+  filter(startDtEarly > 1940-01-01)%>%
+  filter(endDtLate    > 1940-01-01)
+
+# adding back in 2010 data
+p.all <- bind_rows(p.05.17f, p.10)%>%
+  arrange(year, cgPlaId) %>%
+  select(-c(endDtEarly, startDtLate))
+
+# reference pop - 1996, 1997, 1998, 1999
+ALL1990s <- p.all %>% filter(cgPlaId < 2500) 
+
+# 1996 and 1997 datasets
+x19967 <- p.all %>% filter(cgPlaId < 1237) %>%
+            # add columns with julian date
+          dplyr::mutate(startNum = yday(startDtEarly))%>%
+          dplyr::mutate(endNum   = yday(endDtLate))%>%
+            # add column with the number of times something flowered
+          group_by(cgPlaId) %>%
+          dplyr::mutate(phenCt = n()) %>%
+          ungroup() %>%
+          filter(phenCt > 1)
+
+# pedigree data 
+ped967 <- ped %>%
+  filter(cgplaid < 1237) %>%
+  # rename columns
+  dplyr::rename("site" = "siteOfOriginPedigree")%>%
+  dplyr::rename("cgPlaId" = "cgplaid")%>%
+  # recode levels
+  mutate(site = recode(site, 
+                       "AA"      = "aa",
+                       "Aanenson"= "aa",
+                       "Eriley"  = "eri",
+                       "Riley"   = "ri",
+                       "Lf"      = "lf",
+                       "NWLF"    = "nwlf",
+                       "NWlf"    = "nwlf",
+                       "SPP"     = "spp",
+                       "Stevens" = "sap",
+                       "Staff"   = "spp",
+                       "Nessman" = "ness"
+                       ))
+
+# merging data
+phen_19967 <- left_join(x19967, ped967, by = "cgPlaId")%>%
+  # get rid of empty levels - I'm going to ignore this for now, since I actually don't care about site origin. 
+   filter(cgPlaId != 0
+    #    site != "",
+    #    site != "Unknown",
+    #    site != "Tower",
+         ) %>%
+  # add in row position data
+  left_join(., rowpos, by = "cgPlaId")%>%
+  ungroup(year)%>%
+  mutate(dur = endNum - startNum,
+         year = forcats::as_factor(year)) %>%
+  filter(dur < 100) %>%
+  group_by(year)%>%
+  mutate(medianDate = median(startDtEarly)) %>%
+  mutate(dam = as.numeric(startDtEarly - medianDate)) %>%
+  dplyr::mutate(rank = dense_rank(startDtEarly))%>%
+  dplyr::mutate(daf = startDtEarly - min(startDtEarly)) %>%
+  ungroup(year) %>%
+  dplyr::mutate(site = forcats::as_factor(site))
+
+# # saving data for easy access in the future # #  
+# save(phen_19967, file = "phen_dataset.rda")
+
+# Also going to save separate dataframes for 1996 and 1997
+phen_1996 <- phen_19967 %>% filter(cgPlaId < 647)
+# save(phen_1996, file = "phen96_dataset.rda")
+phen_1997 <- phen_19967 %>% filter(646 < cgPlaId & cgPlaId < 1237)
+# save(phen_1997, file = "phen97_dataset.rda")
