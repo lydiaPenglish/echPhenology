e327b534a5047b410ada02e31803caa63ed2cc7f
Lydia English <lenglish@iastate.edu>
2020-03-01 10:19:10 -0600

    edited reprex

--- ggplot_reprex.R
+++ ggplot_reprex.R
@@ -1,13 +1,18 @@
 library(reprex)
 
+
+
 reprex({
   library(tidyverse)
   
   # I've set up this dataframe
   ex <- data.frame(x1 = c(rep("2010", 3), rep("2011", 3), rep("2012", 3)),
-                        x2 = rep(c("2010", "2011", "2012"), 3),
-                        r = c(NA, 0.5, 0.2, NA, NA, 0.1, NA, NA, NA),
-                        n = c(NA, NA, NA, 25, NA, NA, 70, 50, NA))
+                   x2 = rep(c("2010", "2011", "2012"), 3),
+                   r = c(NA, 0.5, 0.2, NA, NA, 0.1, NA, NA, NA),
+                   n = c(NA, NA, NA, 25, NA, NA, 70, 50, NA))
+  
+  ex
+  
   # when I run this graph, with size as an aesthetic, NAs are automatically removed
   ex %>%
       ggplot(aes(x1, x2))+
@@ -18,4 +23,5 @@ reprex({
   ex %>%
       ggplot(aes(x1, x2))+
       geom_point(aes(color = r), size = 6)+
-      geom_text(aes(label = n))})
+      geom_text(aes(label = n))
+  }, venue = "so")
